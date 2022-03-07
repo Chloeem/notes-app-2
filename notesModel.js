66a8e238f22c2342a57eb6e2ad9e0d442b34ad68
Jane Parsons <jane.parsons@waitrose.co.uk>
2022-03-07 16:52:13 +0000

    Jest testing add and reset methods

--- notesModel.js
+++ notesModel.js
@@ -8,6 +8,14 @@ class NotesModel {
     return this.notes
   };
 
+  addNote(note) {
+    //this.getNotes().push(note);
+    this.notes.push(note);
+  };
+
+  reset() {
+    this.notes = [];
+  };
 
 };
 
--- notesModel.test.js
+++ notesModel.test.js
@@ -1,19 +1,40 @@
 const NotesModel = require('./notesModel');
 
 describe(NotesModel, () => {
+    
+    let model;
+    beforeEach(() => {
+      model = new NotesModel 
+    });
 
     describe('#getNotes', () => {
       
-      let model;
-
-      beforeEach(() => {
-        model = new NotesModel 
-      });
-
       it('returns an empty array on creation', () => {
         expect(model.getNotes()).toEqual([])
       });
 
     });
+
+    describe('#addNote', () => {
+      
+        it('add note to array', () => {
+          model.addNote("Buy Milk!");
+          model.addNote("Go to the Gym!");  
+          expect(model.getNotes()).toEqual(["Buy Milk!", "Go to the Gym!"]);
+        });
+  
+    });
+  
+    describe('#reset', () => {
+      
+        it('reset array to empty', () => {
+          model.addNote("Buy Milk!");
+          model.addNote("Go to the Gym!"); 
+          model.reset(); 
+          expect(model.getNotes()).toEqual([]);
+        });
+  
+    });
+  
 
 });    
